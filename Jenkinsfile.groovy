b672ae0e5cea04206904ecda2ae1741d52c3b500
godsmack1d <godsmack1d@gmail.com>
2020-08-18 08:02:11 +0300

    jenkinsfile added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+    tools {
+        maven 'Gradle 5.6.4'
+        jdk 'jdk8'
+    }
+
+//    triggers {
+//        cron('H */8 * * *') // regular builds
+//        pollSCM('* * * * *') // polling for changes, here once a minute
+//    }
+
+    stages {
+        stage('Build') {
+            steps {
+                bat 'gradlew build'
+            }
+        }
+    }
+}
